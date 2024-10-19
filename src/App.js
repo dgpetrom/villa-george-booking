dd824cd8b632f158f3f76d99d663c3b486f31b2a
dgpetrom <dgpetrom@gmail.com>
2024-10-19 10:44:38 -0400

    redirection

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import BookingForm from './BookingForm';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BookingForm from './BookingForm'; // Import your booking form
-import CalendarPage from './CalendarPage'; // Import your calendar page
 
 function App() {
     return (
@@ -13,7 +12,6 @@ function App() {
         <Router>
             <Routes>
                 <Route path="/" element={<BookingForm />} />
-                <Route path="/calendar" element={<CalendarPage />} />
             </Routes>
         </Router>
     );
