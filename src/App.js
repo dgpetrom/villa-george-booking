ccc1eab3915e0c2cc8339479ec84c02561a8c6d7
dgpetrom <dgpetrom@gmail.com>
2024-10-19 14:21:35 -0400

    redirection

--- src/App.js
+++ src/App.js
@@ -9,22 +9,18 @@ const stripePromise = loadStripe('pk_test_51QBgQ1B1SHqUmRkqRJ8DtMcTvmjW1kSX8qyjp
 export default App;
 
 function App() {
-    return (
-        <div className="App">
-            <h1>Villa George Booking</h1>
-            <BookingForm />
-        </div>
-    );
-
     const handlePaymentSuccess = () => {
         // Redirect to booking confirmation page or show success message
         window.location.href = 'https://dgpetrom.github.io/VillaGeorge/booking.html';  // Redirect to your static booking page
     };
-
     return (
-        <Elements stripe={stripePromise}>
-            <PaymentForm amount={5000} currency="usd" onPaymentSuccess={handlePaymentSuccess} />
-        </Elements>
+        <div className="App">
+            <h1>Villa George Booking</h1>
+            <Elements stripe={stripePromise}>
+                <PaymentForm amount={5000} currency="usd" onPaymentSuccess={handlePaymentSuccess} />
+            </Elements>
+            <BookingForm />
+        </div>
     );
 }
 
