0cd652a8845a74f25994a28a19832277f2b08749
dgpetrom <dgpetrom@gmail.com>
2024-10-19 10:00:47 -0400

    redirection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import BookingForm from './BookingForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import BookingForm from './BookingForm'; // Import your booking form
+import CalendarPage from './CalendarPage'; // Import your calendar page
 
 function App() {
     return (
@@ -7,7 +10,12 @@ function App() {
             <h1>Villa George Booking</h1>
             <BookingForm />
         </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<BookingForm />} />
+                <Route path="/calendar" element={<CalendarPage />} />
+            </Routes>
+        </Router>
     );
 }
 
-export default App;
